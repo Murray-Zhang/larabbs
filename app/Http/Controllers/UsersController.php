97836225598400c226e0ed259377d4edee4c8d7b
zhanghuizheng <394401270@qq.com>
2019-09-17 20:01:34 +0800

    访问授权

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,21 +9,28 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
     public function show(User $user)
     {
+
         return view('users.show', compact('user'));
     }
 
     //编辑资料页面
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     //修改个人资料
     public function update(UserRequest $request, User $user, ImageUploadHandler $uploader)
     {
-
+        $this->authorize('update', $user);
         $data = $request->all();
         if($request->avatar){
             $result = $uploader->save($request->avatar, 'avatars', $user->id, '253');
