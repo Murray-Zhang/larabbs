faa2eb9f7c3a02936986bc58a897d5a17c6593bc
zhanghuizheng <394401270@qq.com>
2019-09-18 16:24:05 +0800

    利用代码生成器生成话题topic骨架

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,7 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 
 //个人中心相关路由
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
+
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
