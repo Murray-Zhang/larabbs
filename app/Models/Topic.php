9a8f80a7b52aa7d7231b87489fc4719762af3149
zhanghuizheng <394401270@qq.com>
2019-09-18 18:20:20 +0800

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,4 +5,17 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+
+    //获取话题的所属分类
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    //获取帖子的用户
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
