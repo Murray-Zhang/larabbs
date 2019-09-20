f93ab52ee4036d9b2bfa0943f1b9b9a1c8004b88
zhanghuizheng <394401270@qq.com>
2019-09-20 13:18:31 +0800

    修复xss注入漏洞

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -12,6 +12,8 @@ class TopicObserver
     //在保存数据是生成文章摘要
     public function saving(Topic $topic)
     {
+        //入库前使用purifier 过滤防止xss攻击
+        $topic->body = clean($topic->body, 'user_topic_body');
         //自定义的辅助方法
         $topic->excerpt = make_excerpt($topic->body);
     }
