a184e1fa173c14b7e6a68ba635140ff806fe40bc
zhanghuizheng <394401270@qq.com>
2019-09-21 20:09:18 +0800

    翻译队列话

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 use App\Models\Topic;
 
 // creating, created, updating, updated, saving,
@@ -17,10 +18,15 @@ class TopicObserver
         $topic->body = clean($topic->body, 'user_topic_body');
         //自定义的辅助方法
         $topic->excerpt = make_excerpt($topic->body);
+    }
 
+    //将创建的topic 模型推入到队列中
+    public function saved(Topic $topic)
+    {
         //生成seo友好的url slug
         if (!$topic->slug) {
-            $topic->slug = (new SlugTranslateHandler())->translate($topic->title);
+            //推送任务到队列
+            dispatch(new TranslateSlug($topic));
         }
     }
 }
