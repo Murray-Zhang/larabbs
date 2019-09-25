8ee7d3fc2c44004b2ee0f3e38c5873ad5cdef16f
zhanghuizheng <394401270@qq.com>
2019-09-25 22:02:36 +0800

    用户管理后台

--- config/administrator.php
+++ config/administrator.php
@@ -1,139 +1,69 @@
 <?php
 
 return array(
-
-    /*
-     * Package URI
-     *
-     * @type string
-     */
+// 后台的 URI ⼊⼝
     'uri' => 'admin',
-
-    /*
-     *  Domain for routing.
-     *
-     *  @type string
-     */
+// 后台专属域名，没有的话可以留空
     'domain' => '',
-
-    /*
-     * Page title
-     *
-     * @type string
-     */
-    'title' => config('app.name'),
-
-    /*
-     * The path to your model config directory
-     *
-     * @type string
-     */
+// 应⽤名称，在⻚⾯标题和左上⻆站点名称处显⽰
+    'title' => env('APP_NAME', 'Laravel'),
+// 模型配置信息⽂件存放⽬录
     'model_config_path' => config_path('administrator'),
-
-    /*
-     * The path to your settings config directory
-     *
-     * @type string
-     */
+// 配置信息⽂件存放⽬录
     'settings_config_path' => config_path('administrator/settings'),
-
     /*
-     * The menu structure of the site. For models, you should either supply the name of a model config file or an array of names of model config
-     * files. The same applies to settings config files, except you must prepend 'settings.' to the settings config file name. You can also add
-     * custom pages by prepending a view path with 'page.'. By providing an array of names, you can group certain models or settings pages
-     * together. Each name needs to either have a config file in your model config path, settings config path with the same name, or a path to a
-     * fully-qualified Laravel view. So 'users' would require a 'users.php' file in your model config path, 'settings.site' would require a
-     * 'site.php' file in your settings config path, and 'page.foo.test' would require a 'test.php' or 'test.blade.php' file in a 'foo' directory
-     * inside your view directory.
-     *
-     * @type array
-     *
-     * 	array(
-     *		'E-Commerce' => array('collections', 'products', 'product_images', 'orders'),
-     *		'homepage_sliders',
-     *		'users',
-     *		'roles',
-     *		'colors',
-     *		'Settings' => array('settings.site', 'settings.ecommerce', 'settings.social'),
-     * 		'Analytics' => array('E-Commerce' => 'page.ecommerce.analytics'),
-     *	)
-     */
-    'menu' => [],
-
+    * 后台菜单数组，多维数组渲染结果为多级嵌套菜单。
+    *
+    * 数组⾥的值有三种类型：
+    * 1. 字符串 —— ⼦菜单的⼊⼝，不可访问；
+    * 2. 模型配置⽂件 —— 访问 `model_config_path` ⽬录下的模型⽂件，如 `users` 访问的是 `users.php` 模型配置⽂件；
+    * 3. 配置信息 —— 必须使⽤前缀 `settings.`，对应 `settings_config_path` ⽬录下的⽂件，如：默认设置下，
+    * `settings.site` 访问的是 `administrator/settings/site.php` ⽂件
+    * 4. ⻚⾯⽂件 —— 必须使⽤前缀 `page.`，如：`page.pages.analytics` 对应 `administrator/pages/analytics.php`
+    * 或者是 `administrator/pages/analytics.blade.php` ，两种后缀名皆可
+    *
+    * ⽰例：
+    * [
+    * 'users',
+    * 'E-Commerce' => ['collections', 'products', 'product_images', 'orders'],
+    * 'Settings' => ['settings.site', 'settings.ecommerce', 'settings.social'],
+    * 'Analytics' => ['E-Commerce' => 'page.pages.analytics'],
+    * ]
+    */
+    'menu' => [
+        '⽤⼾与权限' => [
+            'users',
+        ],
+    ],
     /*
-     * The permission option is the highest-level authentication check that lets you define a closure that should return true if the current user
-     * is allowed to view the admin section. Any "falsey" response will send the user back to the 'login_path' defined below.
-     *
-     * @type closure
-     */
+    * 权限控制的回调函数。
+    *
+    * 此回调函数需要返回 true 或 false ，⽤来检测当前⽤⼾是否有权限访问后台。
+    * `true` 为通过，`false` 会将⻚⾯重定向到 `login_path` 选项定义的 URL 中。
+    */
     'permission' => function () {
-        return Auth::check();
+// 只要是能管理内容的⽤⼾，就允许访问后台
+        return Auth::check() && Auth::user()->can('manage_contents');
     },
-
     /*
-     * This determines if you will have a dashboard (whose view you provide in the dashboard_view option) or a non-dashboard home
-     * page (whose menu item you provide in the home_page option)
-     *
-     * @type bool
-     */
+    * 使⽤布尔值来设定是否使⽤后台主⻚⾯。
+    *
+    * 如值为 `true`，将使⽤ `dashboard_view` 定义的视图⽂件渲染⻚⾯；
+    * 如值为 `false`，将使⽤ `home_page` 定义的菜单条⽬来作为后台主⻚。
+    */
     'use_dashboard' => false,
-
-    /*
-     * If you want to create a dashboard view, provide the view string here.
-     *
-     * @type string
-     */
+// 设置后台主⻚视图⽂件，由 `use_dashboard` 选项决定
     'dashboard_view' => '',
-
-    /*
-     * The menu item that should be used as the default landing page of the administrative section
-     *
-     * @type string
-     */
-    'home_page' => '',
-
-    /*
-     * The route to which the user will be taken when they click the "back to site" button
-     *
-     * @type string
-     */
+// ⽤来作为后台主⻚的菜单条⽬，由 `use_dashboard` 选项决定，菜单指的是 `menu` 选项
+    'home_page' => 'users',
+// 右上⻆『返回主站』按钮的链接
     'back_to_site_path' => '/',
-
-    /*
-     * The login path is the path where Administrator will send the user if they fail a permission check
-     *
-     * @type string
-     */
+// 当选项 `permission` 权限检测不通过时，会重定向⽤⼾到此处设置的路径
     'login_path' => 'login',
-
-    /*
-     * The logout path is the path where Administrator will send the user when they click the logout link
-     *
-     * @type string
-     */
-    'logout_path' => false,
-
-    /*
-     * This is the key of the return path that is sent with the redirection to your login_action. Session::get('redirect') will hold the return URL.
-     *
-     * @type string
-     */
+// 允许在登录成功后使⽤ Session::get('redirect') 将⽤⼾重定向到原本想要访问的后台⻚⾯
     'login_redirect_key' => 'redirect',
-
-    /*
-     * Global default rows per page
-     *
-     * @type int
-     */
+// 控制模型数据列表⻚默认的显⽰条⽬
     'global_rows_per_page' => 20,
-
-    /*
-     * An array of available locale strings. This determines which locales are available in the languages menu at the top right of the Administrator
-     * interface.
-     *
-     * @type array
-     */
+// 可选的语⾔，如果不为空，将会在⻚⾯顶部显⽰『选择语⾔』按钮
     'locales' => [],
-
-    'custom_routes_file' => app_path('Http/routes/administrator.php'),
 );
